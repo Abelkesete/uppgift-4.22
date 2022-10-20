f4b72ba4d6c6ee0606492d345e71800b688bc42c
Pingvu <89245290+BaoLanNguyen@users.noreply.github.com>
2022-10-20 13:54:12 +0200

    Add project files.

---
+++ uppgift 4.22/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift_4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i <=30 ; i++)
+            { 
+
+                Console.WriteLine(i);
+            }
+            for (int i2 = 200; i2 >=180 ; i2--)
+            {
+
+                Console.WriteLine(i2);
+            }
+            for (int i3 = 1000; i3 <=1400 ; i3+=50)
+            {
+
+                Console.WriteLine(i3);
+            }
+        }
+    }
+}
